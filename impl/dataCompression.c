24f43c347d61d7e7958fe8b29027cdcb32afa910
smallcat9603 <huyao0107@gmail.com>
2019-12-21 01:09:59 +0900

    minor

--- impl/dataCompression.c
+++ impl/dataCompression.c
@@ -10,32 +10,36 @@
 #include "param.h"
 #include "dataCompression.h"
 
-float jacobi(int);
-int initmax(int,int,int);
-void initmt(int,int);
-void initcomm(int,int,int);
-void sendp(int,int,int);
-void sendp1();
-void sendp2();
-void sendp3();
-
-double fflop(int,int,int);
-double mflops(int,double,double);
-
-static float omega;
-static int npe,id;
+MPI_Datatype myCompress_himeno(void*, int, int, int, int, int, int);
 
 MPI_Datatype
 myCompress_himeno(void* data, int count, int blklen, int stride, int starti, int startj, int startk)
 {
-  float real_value, predict_value1, predict_value2, predict_value3;
+  float real_value, before_value1=-1, before_value2=-1, before_value3=-1, predict_value1, predict_value2, predict_value3;
   int num = 0;
 
   for(int i=0; i<count; i++)
   {
     for(int j=0; j<blklen; j++)
     {
-      real_value = data[starti][startj][startk+blklen]
+      real_value = data[starti][startj][startk+blklen];
+
+      if(before_value3 == -1) before_value3 = real_value;
+      else if(before_value2 == -1) before_value2 = real_value;
+      else if(before_value1 == -1) before_value1 = real_value;
+      else
+      {
+        predict_value1 = before_value1;
+        predict_value2 = 2*before_value1 - before_value2;
+        predict_value3 = 3*before_value1 - 3*before_value2 + before_value3;
+
+        
+
+        before_value3 = before_value2;
+        before_value2 = before_value1;
+        before_value1 = 
+      }
+      
     }
     if(stride == MKMAX)
     {
