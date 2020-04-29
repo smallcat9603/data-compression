413f4e2ac832f46cdf26b794011d6509efba142a
smallcat9603 <huyao0107@gmail.com>
2020-04-30 01:01:37 +0900

    minor

--- impl/dataCompression.h
+++ impl/dataCompression.h
@@ -8,7 +8,7 @@
 #define CT                  5 //compress type for pingpong & himeno & k-means, 0 no compress, 1 mycompress, 2 no-lossy-performance, 3 no-lossy-area, 4 sz, 5 bitwise mycompress
 #define byte_or_bit         2 //1 byte, 2 bit
 #define data_num            8192 //pingpong
-#define filename            "dataset/testfloat_8_8_128" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
+#define filename            "dataset/obs_info" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
 #define suffix              ".txt" //k-means
 #define output_suffix       "_output_" //k-means
 #define clusters            100 //k-means
