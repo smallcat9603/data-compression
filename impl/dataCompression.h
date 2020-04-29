19515bae90ca87dfe8754991ec63236d2702eda7
smallcat9603 <huyao0107@gmail.com>
2020-04-30 00:27:21 +0900

    minor

--- impl/dataCompression.h
+++ impl/dataCompression.h
@@ -6,7 +6,7 @@
 #define relBoundRatio       0.01
 #define pw_relBoundRatio    0.01    
 #define CT                  5 //compress type for pingpong & himeno & k-means, 0 no compress, 1 mycompress, 2 no-lossy-performance, 3 no-lossy-area, 4 sz, 5 bitwise mycompress
-#define byte_or_bit         1 //1 byte, 2 bit
+#define byte_or_bit         2 //1 byte, 2 bit
 #define data_num            8192 //pingpong
 #define filename            "dataset/num_plasma" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
 #define suffix              ".txt" //k-means
