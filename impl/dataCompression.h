0e5d18cd6e253197c95ce0e04957485f70934314
smallcat9603 <huyao0107@gmail.com>
2020-05-06 00:20:19 +0900

    minor

--- impl/dataCompression.h
+++ impl/dataCompression.h
@@ -8,9 +8,9 @@
 #define CT                  1 //compress type for pingpong & himeno & k-means, 0 no compress, 1 mycompress, 2 no-lossy-performance, 3 no-lossy-area, 4 sz, 5 bitwise mycompress
 #define byte_or_bit         2 //1 byte, 2 bit
 #define data_num            8192 //pingpong
-#define filename            "dataset/num_plasma" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
-#define suffix              ".txt" //k-means
-#define output_suffix       "_output_" //k-means
+#define filename            "dataset/obs_info" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
+#define suffix              ".txt" //k-means, ".txt"
+#define output_suffix       "_output_s_" //k-means, "_output_", "_output_s_"
 #define clusters            100 //k-means
 
 double* myDecompress_bitwise_double(unsigned char*, int, int);
