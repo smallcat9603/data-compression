7b08481d9cf707041aa3f164a50faf63096664c8
smallcat9603 <huyao0107@gmail.com>
2020-05-10 00:07:19 +0900

    minor

--- himeno_compress_koi/param.h
+++ himeno_compress_koi/param.h
@@ -4,9 +4,9 @@
 #define MX0     65
 #define MY0     65
 #define MZ0     129
-#define MIMAX     35
-#define MJMAX     35
-#define MKMAX     67
-#define NDX0      2
-#define NDY0      2
-#define NDZ0      2
+#define MIMAX     19
+#define MJMAX     19
+#define MKMAX     35
+#define NDX0      4
+#define NDY0      4
+#define NDZ0      4
