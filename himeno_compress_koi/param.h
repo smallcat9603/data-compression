7c27a7b7feadabe9d7d4271a8c2ed55574eac623
smallcat9603 <huyao0107@gmail.com>
2020-05-09 22:22:01 +0900

    add himeno koi

---
+++ himeno_compress_koi/param.h
@@ -0,0 +1,12 @@
+/*
+ *
+ */
+#define MX0     65
+#define MY0     65
+#define MZ0     129
+#define MIMAX     35
+#define MJMAX     35
+#define MKMAX     67
+#define NDX0      2
+#define NDY0      2
+#define NDZ0      2
