4af2c7230f1f9ceb99c4c4e255c1e0a3b1496631
smallcat9603 <huyao0107@gmail.com>
2020-05-24 22:58:49 +0900

    minor

--- impl/dataCompression.h
+++ impl/dataCompression.h
@@ -1,16 +1,16 @@
 /*
  *
  */
-#define absErrBound         0.00001 //default 0.0001=2^{-12} (-13?), 0.000001=2^{-20}, 0.00001=2^{-16}, 0.001=2^{-10}, 0.01=2^{-7}
-#define absErrBound_binary  16 //bitwise, SZ, equal to above
+#define absErrBound         0.000001 //default 0.0001=2^{-12} (-13?), 0.000001=2^{-20}, 0.00001=2^{-16}, 0.001=2^{-10}, 0.01=2^{-7}
+#define absErrBound_binary  20 //bitwise, SZ, equal to above
 #define relBoundRatio       0.01
 #define pw_relBoundRatio    0.01    
 #define CT                  6 //compress type for pingpong & himeno & k-means, 0 no compress, 1 mycompress, 2 no-lossy-performance, 3 no-lossy-area, 4 sz, 5 bitwise mycompress, 6 bitwise no prediction
 #define byte_or_bit         2 //1 byte, 2 bit
 #define data_num            8192 //pingpong
-#define filename            "dataset/test" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
+#define filename            "dataset/obs_info" //pingpong, k-means, "input", "testfloat_8_8_128", "testdouble_8_8_128", "testdouble_8_8_8_128", test, obs_info, num_plasma
 #define suffix              ".txt" //k-means, ".txt"
-#define output_suffix       "_output_s_" //k-means, "_output_", "_output_s_"
+#define output_suffix       "_output_" //k-means, "_output_", "_output_s_"
 #define clusters            100 //k-means
 
 double* myDecompress_bitwise_double_np(unsigned char*, int, int);
