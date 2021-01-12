647e6a179f6ff5da24a0378b40c89f5603d51ce3
smallcat9603 <huyao0107@gmail.com>
2021-01-12 18:07:18 +0900

    add hamming encode test

---
+++ impl/hamming.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <math.h>
+
+//N is the max number of data bits
+#define N 100
+
+int HmLength(int k); //hamming check bits
+void Encode(char* data, char* c, int k, int r); //value of each hamming check bit
+
+void main()
+{
+  int k = 0, r = 0, dnum = 0, cnum = 0;
+  char data[N];
+  char c[N];
+
+  printf("Input data to encode: \n");
+
+  for(k = 0; k < N; k++)
+  {
+    data[k] = getchar();
+    if(data[k] != '0' && data[k] != '1') break;
+  }
+
+  r = HmLength(k);
+  printf("k = %d, r = %d, length of hamming code is %d \n", k, r, k+r);
+  Encode(data, c, k, r); //check c, data bits k, check bits r
+  printf("hamming code is ");
+  for(int j = 1; j < r+k+1; j++)
+  {
+    if(j == (int)pow(2, cnum))
+    {
+      printf("%c", c[cnum]);
+      cnum++;
+    }
+    else
+    {
+      printf("%c", data[dnum]);
+      dnum++;
+    }
+  }
+  getchar();
+}
+
+void Encode(char* data, char* c, int k, int r)
+{
+  for(int i=0; i<r; i++)
+  {
+    int sum = 0, dnum = 0, cnum = 0;
+    for(int j=1; j<r+k+1; j++)
+    {
+      if(j == (int)pow(2, cnum))
+      {
+        cnum++;
+      }
+      else
+      {
+        int x = 0, y = 0;
+        x = pow(2, i);
+        y = j%(x*2);
+        x = y/x;
+        sum += data[dnum]*x;
+        dnum++;
+      }
+      
+    }
+    c[i] = sum%2 == 0?'0':'1';
+  }
+}
+
+int HmLength(int k)
+{
+  int r = 0, flag = 1;
+  while(flag != 0)
+  {
+    int temp = 0;
+    temp = pow(2, r);
+    temp = temp - 1;
+    flag = (temp-r-k<0);
+    r++;
+  }
+  return r-1;
+}
