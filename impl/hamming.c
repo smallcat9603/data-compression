7a85b75c4fff0f87e4f9bf871f77da4520ef79e1
smallcat9603 <huyao0107@gmail.com>
2021-01-26 03:01:38 +0900

    add hamming_decode

--- impl/hamming.c
+++ impl/hamming.c
@@ -13,6 +13,9 @@ void hamming_print(char* data, char* c, int k, int r); //print hamming secded
 void hamming_rectify(char* data, char* c, int k, int r, int error_bit_pos); //rectify error bit if one error (except parity bit)
 void cast_bits_to_char(unsigned char* bits, char* data, int bytes); //1 bit --> char '0' or '1' (8 bits)
 void hamming_encode(unsigned char* bits, char** c, int bytes, int* r); //data bits --> char[]
+int hamming_decode(unsigned char* bits, char* c, int bytes, int r);
+void hamming_verify_bit(unsigned char* bits, char* c, int bytes, int r, char* v);
+void hamming_rectify_bit(unsigned char* bits, char* c, int bytes, int r, int error_bit_pos); //rectify error bit if one error (except parity bit)
 
 void main()
 {
@@ -30,6 +33,14 @@ void main()
   //   printf("%c", d[i]);
   // }
   // printf("\n");
+  // data[0] ^= 1 << 7;
+  // c[0] = '0';
+  // c[1] = '1';
+  // c[2] = '0';
+  // c[3] = '1';
+  // c[4] = '0';
+  // c[5] = '0';
+  // hamming_decode(data, c, 2, 5);
   // return;
 
   printf("Input data to encode: ");
@@ -119,7 +130,7 @@ void hamming_code(char* data, char* c, int k, int r)
         int x = pow(2, i);
         int y = j%(x*2);
         x = y/x;
-        sum += data[dnum]*x;
+        sum += (data[dnum]-'0')*x;
         dnum++;
       }
     }
@@ -130,11 +141,11 @@ void hamming_code(char* data, char* c, int k, int r)
   int sum = 0;
   for(int i=0; i<k; i++)
   {
-    sum += data[i];
+    sum += data[i] - '0';
   }
   for(int i=0; i<r; i++)
   {
-    sum += c[i];
+    sum += c[i] - '0';
   }
   c[r] = sum%2 + '0';
 }
@@ -170,7 +181,7 @@ void hamming_verify(char* data, char* c, int k, int r, char* v)
         int x = pow(2, i);
         int y = j%(x*2);
         x = y/x;
-        sum += data[dnum]*x;
+        sum += (data[dnum]-'0')*x;
         dnum++;
       }
     }
@@ -180,11 +191,11 @@ void hamming_verify(char* data, char* c, int k, int r, char* v)
   int sum = 0;
   for(int i=0; i<k; i++)
   {
-    sum += data[i];
+    sum += data[i] - '0';
   }
   for(int i=0; i<r; i++)
   {
-    sum += c[i];
+    sum += c[i] - '0';
   }
   v[r] = sum%2 == (c[r]-'0')?'0':'1';
 }
@@ -307,4 +318,111 @@ void hamming_encode(unsigned char* bits, char** c, int bytes, int* r)
   *r = hmLength(bytes*8);
   *c = (char*) malloc(sizeof(char)*(*r+1));
   hamming_code(data, *c, bytes*8, *r);
+}
+
+int hamming_decode(unsigned char* bits, char* c, int bytes, int r)
+{
+  char v[r+1];
+  hamming_verify_bit(bits, c, bytes, r, v);
+
+  int error_bit_pos = 0; // 1 for most left bit
+  int error_type = error_info(v, r, &error_bit_pos);  
+  switch(error_type)
+  {
+    case 0:
+      printf("no error\n");
+      break;
+    case 1:
+      printf("two-bit error\n");
+      break;
+    case 2:
+      printf("parity error\n");
+      c[r] = c[r] == '0'?'1':'0';
+      break;
+    case 3:
+      printf("one bit error: pos = %d\n", error_bit_pos);
+      hamming_rectify_bit(bits, c, bytes, r, error_bit_pos);
+      break;
+    default:
+      printf("ERROR");
+  }
+
+  return error_type;
+}
+
+//hamming verify --> v
+void hamming_verify_bit(unsigned char* bits, char* c, int bytes, int r, char* v)
+{
+  for(int i=0; i<r; i++)
+  {
+    int sum = 0, bnum = 0, cnum = 0;
+    for(int j=1; j<r+bytes*8+1; j++)
+    {
+      if(j == (int)pow(2, cnum))
+      {
+        cnum++;
+      }
+      else
+      {
+        int x = pow(2, i);
+        int y = j%(x*2);
+        x = y/x;
+        int bits_num = bnum/8;
+        int bit_num = bnum%8;       
+        sum += ((bits[bits_num] >> (7 - bit_num)) & 1) * x;
+        bnum++;
+      }
+    }
+    v[i] = sum%2 == (c[i]-'0')?'0':'1';
+  }
+
+  int sum = 0;
+  for(int i=0; i<bytes; i++)
+  {
+    for(int j=0; j<8; j++)
+    {
+      sum += (bits[i] >> j) & 1;
+    }
+  }
+  for(int i=0; i<r; i++)
+  {
+    sum += c[i] - '0';
+  }
+  v[r] = sum%2 == (c[r]-'0')?'0':'1';
+}
+
+//rectify error bit if one error (except parity bit)
+void hamming_rectify_bit(unsigned char* bits, char* c, int bytes, int r, int error_bit_pos)
+{
+  int bnum = 0, cnum = 0;
+
+  for(int j=1; j<r+bytes*8+1; j++)
+  {
+    if(j == (int)pow(2, cnum))
+    {   
+      if(j == error_bit_pos)
+      {
+        c[cnum] = c[cnum] == '0'?'1':'0';
+        break;
+      }
+      else
+      {
+        cnum++;
+      }  
+    }
+    else
+    {  
+      if(j == error_bit_pos)
+      {
+        int bits_num = bnum/8;
+        int bit_num = bnum%8;
+        bits[bits_num] ^= 1 << (7 - bit_num);
+        break;
+      }
+      else
+      {
+        bnum++;
+      } 
+    }
+  }
 }
