236347ba6b30c1d77ba7fa59a483c8302ade1035
smallcat9603 <huyao0107@gmail.com>
2022-10-25 14:46:21 +0900

    make

--- digitroundingZ-master/examples/digit_CompDecomp.c
+++ digitroundingZ-master/examples/digit_CompDecomp.c
@@ -5,10 +5,14 @@
 #include <rw.h>
 #include "libdround.h"
 
+#include <time.h>
+
 int main(int argc, char* argv[])
 {
 	char oriFilePath[640], cmpFile[640], outputFilePath[640];
  	int prec;
+
+	double start_time_comp, end_time_comp, start_time_decomp, end_time_decomp;
 
   	if(argc < 3)
   	{
@@ -34,20 +38,29 @@ int main(int argc, char* argv[])
 	void* out = NULL;
 	if(strcmp(type, "-f")==0)
 	{
+		start_time_comp = clock();
 		unsigned char* compressBytes = dround_compress(DROUND_FLOAT, data, nbEle, prec, &outSize);
+		end_time_comp = clock();
 		writeByteData(compressBytes, outSize, cmpFile, &status);
+		start_time_decomp = clock();
 		out = dround_decompress(DROUND_FLOAT, compressBytes, nbEle, outSize);
+		end_time_decomp = clock();
 		writeFloatData_inBytes(out, nbEle, outputFilePath, &status);
 	}
 	else if(strcmp(type, "-d")==0)
 	{
+		start_time_comp = clock();
 		unsigned char* compressBytes = dround_compress(DROUND_DOUBLE, data, nbEle, prec, &outSize);
+		end_time_comp = clock();
 		writeByteData(compressBytes, outSize, cmpFile, &status);
+		start_time_decomp = clock();
 		out = dround_decompress(DROUND_DOUBLE, compressBytes, nbEle, outSize);
+		end_time_decomp = clock();
 		writeDoubleData_inBytes(out, nbEle, outputFilePath, &status);
 	}
 
-
+	printf("Compression time: %f sec \n", (end_time_comp-start_time_comp)/CLOCKS_PER_SEC); 
+	printf("Decompression time: %f sec \n", (end_time_decomp-start_time_decomp)/CLOCKS_PER_SEC); 
 	
 	//for(i=0;i<10;i++)
 	//	printf("out[%d]=%f\n", i, out[i]);	
